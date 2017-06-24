65f48a63a8e43ca041462a9c2280435ad15779a6
Alex Cruz <mazdarider23@gmail.com>
2017-06-24 16:13:35 +0200

    DerpQuest: Add SELinux switch
    Squashed commits:
    
    commit ce05f7dc7ad333b1234fee001e54ebd3e695634e
    Author: Alex Cruz <mazdarider23@gmail.com>
    Date:   Thu Dec 22 18:05:55 2016 +0100
    
        Selinux Switch
    
        Change-Id: Idd38f844f94bdc81e2b690682c73e468156ea10b
    
    commit 6b29cf14f8c25f8ee476d86225da3faa7c632a21
    Author: LorDClockaN <lordclockan@gmail.com>
    Date:   Thu Dec 22 18:11:05 2016 +0100
    
        Selinux Switch: Show toast only for permissive on boot
    
        Change-Id: I4e0589e09904332cf0dead5fe2bc9f416caf812e
    
    commit 217411c19ac0c0ac0bfd47b15d00f3d33406fa57
    Author: Meninblack007 <sanyam.53jain@gmail.com>
    Date:   Thu Dec 22 18:57:39 2016 +0100
    
        Settings: Fix selinux switch (#601)
    
        * Cleanup whitespaces
        * Use SharedPreference to save values
    
        Change-Id: I8dd13c3d2f6a16e4633933b5d750c9db412aa8e5
        Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>
    
    Author: SpiritCroc <spiritcroc@gmail.com>
    
        Use our SuShell utils, handle su denied, use boolean in
        shared pref, enhance our root extras fragment for
        initialization, ...
    
    Change-Id: I2469c45a794f960102534efc97f2e0c26e812987
    
    commit 75b568bfb11c70f6d386d7d27cc34aff9d550237
    Author: SpiritCroc <spiritcroc@gmail.com>
    
    @idoybh (DerpFest) edits: Adapt, organize, improve some code
    
    Co-authored-by: SpiritCroc <spiritcroc@gmail.com>
    Co-authored-by: Ido Ben-Hur <idoybh2@gmail.com>

--- src/com/derpquest/settings/fragments/MiscSettings.java
+++ src/com/derpquest/settings/fragments/MiscSettings.java
@@ -17,7 +17,10 @@ package com.derpquest.settings.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SELinux;
 import android.provider.Settings;
 import android.text.TextUtils;
 import androidx.preference.ListPreference;
@@ -28,10 +31,13 @@ import androidx.preference.PreferenceCategory;
 import androidx.preference.SwitchPreference;
 import com.android.internal.util.aosip.DeviceUtils;
 import com.android.settings.R;
+import android.util.Log;
 
 import com.derpquest.settings.preferences.AppMultiSelectListPreference;
 import com.derpquest.settings.preferences.ScrollAppsViewPreference;
 import com.derpquest.settings.preferences.SystemSettingSwitchPreference;
+import com.derpquest.settings.utils.SuShell;
+import com.derpquest.settings.utils.SuTask;
 
 import java.util.Arrays;
 import java.util.ArrayList;
@@ -44,14 +50,21 @@ import com.android.settings.SettingsPreferenceFragment;
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
+    private static final String TAG = "MiscSettings";
+
     private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
     private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
     private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
     private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
     private static final String SYSTEM_PROXI_CHECK_ENABLED = "system_proxi_check_enabled";
+    private static final String SELINUX_CATEGORY = "selinux";
+    private static final String PREF_SELINUX_MODE = "selinux_mode";
+    private static final String PREF_SELINUX_PERSISTENCE = "selinux_persistence";
 
     private AppMultiSelectListPreference mAspectRatioAppsSelect;
     private ScrollAppsViewPreference mAspectRatioApps;
+    private SwitchPreference mSelinuxMode;
+    private SwitchPreference mSelinuxPersistence;
 
 
     @Override
@@ -90,6 +103,18 @@ public class MiscSettings extends SettingsPreferenceFragment implements
       if (!DeviceUtils.deviceSupportsProximitySensor(getActivity()) || !supportPowerButtonProxyCheck) {
           getPreferenceScreen().removePreference(proxyCheckPreference);
       }
+      // SELinux
+      Preference selinuxCategory = findPreference(SELINUX_CATEGORY);
+      mSelinuxMode = (SwitchPreference) findPreference(PREF_SELINUX_MODE);
+      mSelinuxMode.setChecked(SELinux.isSELinuxEnforced());
+      mSelinuxMode.setOnPreferenceChangeListener(this);
+
+      mSelinuxPersistence =
+          (SwitchPreference) findPreference(PREF_SELINUX_PERSISTENCE);
+      mSelinuxPersistence.setOnPreferenceChangeListener(this);
+      mSelinuxPersistence.setChecked(getContext()
+          .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE)
+          .contains(PREF_SELINUX_MODE));
     }
 
     @Override
@@ -107,6 +132,14 @@ public class MiscSettings extends SettingsPreferenceFragment implements
               Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, "");
         }
         return true;
+      } else if (preference == mSelinuxMode) {
+        boolean enabled = (Boolean) objValue;
+        new SwitchSelinuxTask(getActivity()).execute(enabled);
+        setSelinuxEnabled(enabled, mSelinuxPersistence.isChecked());
+        return true;
+      } else if (preference == mSelinuxPersistence) {
+        setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) objValue);
+        return true;
       }
       return false;
     }
@@ -114,5 +147,44 @@ public class MiscSettings extends SettingsPreferenceFragment implements
     @Override
     public int getMetricsCategory() {
       return MetricsProto.MetricsEvent.OWLSNEST;
+    }
+
+    private void setSelinuxEnabled(boolean status, boolean persistent) {
+      SharedPreferences.Editor editor = getContext()
+          .getSharedPreferences("selinux_pref", Context.MODE_PRIVATE).edit();
+      if (persistent) {
+        editor.putBoolean(PREF_SELINUX_MODE, status);
+      } else {
+        editor.remove(PREF_SELINUX_MODE);
+      }
+      editor.apply();
+      mSelinuxMode.setChecked(status);
+    }
+
+    private class SwitchSelinuxTask extends SuTask<Boolean> {
+      public SwitchSelinuxTask(Context context) {
+        super(context);
+      }
+      @Override
+      protected void sudoInBackground(Boolean... params) throws SuShell.SuDeniedException {
+        if (params.length != 1) {
+          Log.e(TAG, "SwitchSelinuxTask: invalid params count");
+          return;
+        }
+        if (params[0]) {
+          SuShell.runWithSuCheck("setenforce 1");
+        } else {
+          SuShell.runWithSuCheck("setenforce 0");
+        }
+      }
+
+      @Override
+      protected void onPostExecute(Boolean result) {
+        super.onPostExecute(result);
+        if (!result) {
+          // Did not work, so restore actual value
+          setSelinuxEnabled(SELinux.isSELinuxEnforced(), mSelinuxPersistence.isChecked());
+        }
+      }
     }
 }
