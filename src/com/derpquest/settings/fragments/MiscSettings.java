47ddd581a1078b8d662b16633c0eddf710d57a50
maxwen <max.weninger@gmail.com>
2018-06-30 14:07:37 +0530

    DerpQuest: Make aspect ratio hack configable on app level [2/2]
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/derpquest/settings/fragments/MiscSettings.java
+++ src/com/derpquest/settings/fragments/MiscSettings.java
@@ -18,21 +18,91 @@ package com.derpquest.settings.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.os.Bundle;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.SwitchPreference;
+import android.provider.Settings;
+import android.text.TextUtils;
 import com.android.settings.R;
+import com.derpquest.settings.preferences.AppMultiSelectListPreference;
+import com.derpquest.settings.preferences.ScrollAppsViewPreference;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 
 import com.android.settings.SettingsPreferenceFragment;
 
-public class MiscSettings extends SettingsPreferenceFragment {
+public class MiscSettings extends SettingsPreferenceFragment implements
+        OnPreferenceChangeListener {
+
+    private static final String KEY_ASPECT_RATIO_APPS_ENABLED = "aspect_ratio_apps_enabled";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST = "aspect_ratio_apps_list";
+    private static final String KEY_ASPECT_RATIO_CATEGORY = "aspect_ratio_category";
+    private static final String KEY_ASPECT_RATIO_APPS_LIST_SCROLLER = "aspect_ratio_apps_list_scroller";
+
+    private AppMultiSelectListPreference mAspectRatioAppsSelect;
+    private ScrollAppsViewPreference mAspectRatioApps;
+
 
     @Override
     public void onCreate(Bundle icicle) {
-        super.onCreate(icicle);
+      super.onCreate(icicle);
 
-        addPreferencesFromResource(R.xml.derpquest_settings_misc);
+      addPreferencesFromResource(R.xml.derpquest_settings_misc);
+
+      final PreferenceCategory aspectRatioCategory =
+          (PreferenceCategory) getPreferenceScreen().findPreference(KEY_ASPECT_RATIO_CATEGORY);
+      final boolean supportMaxAspectRatio =
+          getResources().getBoolean(com.android.internal.R.bool.config_haveHigherAspectRatioScreen);
+      if (!supportMaxAspectRatio) {
+          getPreferenceScreen().removePreference(aspectRatioCategory);
+      } else {
+        mAspectRatioAppsSelect =
+            (AppMultiSelectListPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST);
+        mAspectRatioApps =
+            (ScrollAppsViewPreference) findPreference(KEY_ASPECT_RATIO_APPS_LIST_SCROLLER);
+        final String valuesString = Settings.System.getString(getContentResolver(),
+            Settings.System.OMNI_ASPECT_RATIO_APPS_LIST);
+        List<String> valuesList = new ArrayList<String>();
+        if (!TextUtils.isEmpty(valuesString)) {
+          valuesList.addAll(Arrays.asList(valuesString.split(":")));
+          mAspectRatioApps.setVisible(true);
+          mAspectRatioApps.setValues(valuesList);
+        } else {
+          mAspectRatioApps.setVisible(false);
+        }
+        mAspectRatioAppsSelect.setValues(valuesList);
+        mAspectRatioAppsSelect.setOnPreferenceChangeListener(this);
+      }
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+      if (preference == mAspectRatioAppsSelect) {
+        Collection<String> valueList = (Collection<String>) objValue;
+        mAspectRatioApps.setVisible(false);
+        if (valueList != null) {
+          Settings.System.putString(getContentResolver(),
+              Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, TextUtils.join(":", valueList));
+          mAspectRatioApps.setVisible(true);
+          mAspectRatioApps.setValues(valueList);
+        } else {
+          Settings.System.putString(getContentResolver(),
+              Settings.System.OMNI_ASPECT_RATIO_APPS_LIST, "");
+        }
+        return true;
+      }
+      return false;
     }
 
     @Override
     public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.OWLSNEST;
+      return MetricsProto.MetricsEvent.OWLSNEST;
     }
 }
