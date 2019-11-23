20cc1e30090157b93ad7e331057a1c265790aa0d
beanstown106 <nbenis106@gmail.com>
2019-11-23 18:54:59 +0530

    [SQUASH] DerpQuest: Custom Carrier Label & Carrier Label Placement [2/2]
    Includes the following commits:
    
    commit 38736ad6d918ce42c53156f36d605400db94c072
    Author: beanstown106 <nbenis106@gmail.com>
    Date:   Sat Nov 23 18:54:59 2019 +0530
    
        DerpQuest: Custom Carrier Label & Carrier Label Placement [2/2]
    
        Credit/Thanks to @Altaf-Mahdi @beanstown106 and @martincz
    
        Change-Id: Ia7b29f85d7a1286bd15dc4427727a683e133ab3f
    
    commit c5825e5970797f8c04b6a183d00ba8c273d6f253
    Author: Anushek Prasal <anushekprasal@gmail.com>
    Date:   Sun Aug 9 03:35:06 2020 +0530
    
        DerpQuest: Use MasterSwitch for carrier label [2/2]
    
        Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>
    
    commit 6a0e81d01f95097c300c21046d64ba066ee7d8d6
    Author: Alex <du.alexcruz@gmail.com>
    Date:   Thu Feb 21 21:16:02 2019 +0100
    
        DerpQuest: Notch friendly carrier label [2/2]
    
    commit ec7e1ec6b5e573cd4c41a669c4554d1bd9f72621
    Author: Alex <du.alexcruz@gmail.com>
    Date:   Tue Feb 5 17:17:09 2019 -0600
    
        DerpQuest: Carrier label: Ensure the dialog matches other dialogs
    
        Change-Id: I172fc3cba036dded75585ba444272855b4a912b9
    
    commit 156eef4e9dac5fed6e96295c8d9b218ebfb48bbb
    Author: Alex <du.alexcruz@gmail.com>
    Date:   Thu Jan 31 19:17:58 2019 -0600
    
        DerpQuest: Set a text length on the carrier label
    
        Up til now this has been gone unnoticed. Without a text limit the user
        could very easily blow up the statusbar to the point that is not usable.
    
        Change-Id: I2813af9e12c6d29e020669e0626faec8422fedbe
    
    Co-authored-by: Anushek Prasal <anushekprasal@gmail.com>
    Co-authored-by: Alex <du.alexcruz@gmail.com>
    Change-Id: Ia7b29f85d7a1286bd15dc4427727a683e133ab3f
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

---
+++ src/com/derpquest/settings/fragments/statusbar/CustomCarrierLabel.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2020 Havoc-OS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.derpquest.settings.fragments.statusbar;
+
+import android.app.AlertDialog;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.text.InputFilter;
+import android.text.Spannable;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.derp.derpUtils;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.derp.support.preference.SystemSettingListPreference;
+
+public class CustomCarrierLabel extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener, CompoundButton.OnCheckedChangeListener {
+
+    public static final String TAG = "CarrierLabel";
+    private static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+    private static final String KEY_CARRIER_LABEL = "carrier_label_location";
+
+    private PreferenceScreen mCustomCarrierLabel;
+    private String mCustomCarrierLabelText;
+    private SystemSettingListPreference mShowCarrierLabel;
+
+    private TextView mTextView;
+    private View mSwitchBar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        addPreferencesFromResource(R.xml.custom_carrier_label);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mCustomCarrierLabel = (PreferenceScreen) findPreference(CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
+
+        mShowCarrierLabel = (SystemSettingListPreference) findPreference(KEY_CARRIER_LABEL);
+        int showCarrierLabel = Settings.System.getInt(resolver,
+                Settings.System.CARRIER_LABEL_LOCATION, 0);
+        CharSequence[] NonNotchEntries = {
+            getResources().getString(R.string.show_carrier_keyguard),
+            getResources().getString(R.string.show_carrier_statusbar),
+            getResources().getString(R.string.show_carrier_enabled)
+        };
+        CharSequence[] NotchEntries = {
+            getResources().getString(R.string.show_carrier_keyguard)
+        };
+        CharSequence[] NonNotchValues = {"0", "1" , "2"};
+        CharSequence[] NotchValues = {"0"};
+        mShowCarrierLabel.setEntries(derpUtils.hasNotch(getActivity()) ? NotchEntries : NonNotchEntries);
+        mShowCarrierLabel.setEntryValues(derpUtils.hasNotch(getActivity()) ? NotchValues : NonNotchValues);
+        mShowCarrierLabel.setValue(String.valueOf(showCarrierLabel));
+        mShowCarrierLabel.setSummary(mShowCarrierLabel.getEntry());
+        mShowCarrierLabel.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        final View view = LayoutInflater.from(getContext()).inflate(R.layout.master_setting_switch, container, false);
+        ((ViewGroup) view).addView(super.onCreateView(inflater, container, savedInstanceState));
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        boolean enabled = Settings.System.getInt(getContentResolver(),
+                Settings.System.CARRIER_LABEL_ENABLED, 1) == 1;
+
+        mTextView = view.findViewById(R.id.switch_text);
+        mTextView.setText(getString(enabled ?
+                R.string.switch_on_text : R.string.switch_off_text));
+
+        mSwitchBar = view.findViewById(R.id.switch_bar);
+        Switch switchWidget = mSwitchBar.findViewById(android.R.id.switch_widget);
+        switchWidget.setChecked(enabled);
+        switchWidget.setOnCheckedChangeListener(this);
+        mSwitchBar.setActivated(enabled);
+        mSwitchBar.setOnClickListener(v -> {
+            switchWidget.setChecked(!switchWidget.isChecked());
+            mSwitchBar.setActivated(switchWidget.isChecked());
+        });
+
+        mCustomCarrierLabel.setEnabled(enabled);
+        mShowCarrierLabel.setEnabled(enabled);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+        Settings.System.putInt(getContentResolver(),
+                Settings.System.CARRIER_LABEL_ENABLED, isChecked ? 1 : 0);
+        mTextView.setText(getString(isChecked ? R.string.switch_on_text : R.string.switch_off_text));
+        mSwitchBar.setActivated(isChecked);
+
+        mCustomCarrierLabel.setEnabled(isChecked);
+        mShowCarrierLabel.setEnabled(isChecked);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+ 		ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mShowCarrierLabel) {
+            int value = Integer.parseInt((String) newValue);
+            updateCarrierLabelSummary(value);
+            return true;
+        }
+        return false;
+    }
+
+    private void updateCarrierLabelSummary(int value) {
+        Resources res = getResources();
+
+        if (value == 0) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_keyguard));
+        } else if (value == 1) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_statusbar));
+        } else if (value == 2) {
+            mShowCarrierLabel.setSummary(res.getString(R.string.show_carrier_both));
+        }
+    }
+
+    public boolean onPreferenceTreeClick(Preference preference) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        boolean value;
+        if (preference == mCustomCarrierLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+            LinearLayout container = new LinearLayout(getActivity());
+            container.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+            lp.setMargins(55, 20, 55, 20);
+            final EditText input = new EditText(getActivity());
+            int maxLength = 25;
+            input.setLayoutParams(lp);
+            input.setGravity(android.view.Gravity.TOP| Gravity.START);
+            input.setText(TextUtils.isEmpty(mCustomCarrierLabelText) ? "" : mCustomCarrierLabelText);
+            input.setFilters(new InputFilter[] {new InputFilter.LengthFilter(maxLength)});
+            container.addView(input);
+            alert.setView(container);
+            alert.setPositiveButton(getString(android.R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String value = ((Spannable) input.getText()).toString().trim();
+                            Settings.System.putString(resolver, Settings.System.CUSTOM_CARRIER_LABEL, value);
+                            updateCustomLabelTextSummary();
+                            Intent i = new Intent();
+                            i.setAction(Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED);
+                            getActivity().sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getString(android.R.string.cancel), null);
+            alert.show();
+            return true;
+        }
+        return false;
+    }
+
+    private void updateCustomLabelTextSummary() {
+        mCustomCarrierLabelText = Settings.System.getString(
+            getContentResolver(), Settings.System.CUSTOM_CARRIER_LABEL);
+        if (TextUtils.isEmpty(mCustomCarrierLabelText)) {
+            mCustomCarrierLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomCarrierLabel.setSummary(mCustomCarrierLabelText);
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DERP;
+    }
+}
--- src/com/derpquest/settings/fragments/statusbar/StatusBarItems.java
+++ src/com/derpquest/settings/fragments/statusbar/StatusBarItems.java
@@ -16,12 +16,18 @@
 
 package com.derpquest.settings.fragments.statusbar;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
-import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
 
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -32,6 +38,10 @@ import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import com.derp.support.preference.SecureSettingMasterSwitchPreference;
+import com.derp.support.preference.SwitchPreference;
+import com.derp.support.preference.SystemSettingMasterSwitchPreference;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -39,14 +49,41 @@ import java.util.List;
 public class StatusBarItems extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String CARRIER_LABEL = "carrier_label_enabled";
+
+	private SystemSettingMasterSwitchPreference mCarrierLabel;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.status_bar_items);
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        updateMasterPrefs();
     }
 
+    private void updateMasterPrefs() {
+        mCarrierLabel = (SystemSettingMasterSwitchPreference) findPreference(CARRIER_LABEL);
+        mCarrierLabel.setChecked((Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.CARRIER_LABEL_ENABLED, 1) == 1));
+        mCarrierLabel.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mCarrierLabel) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.CARRIER_LABEL_ENABLED, value ? 1 : 0);
+            return true;
+        }
         return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateMasterPrefs();
     }
 
     @Override
