82ab038458c29eec13eac2727cd9f3f067d94806
Ido Ben-Hur <idoybh2@gmail.com>
2019-11-28 15:27:23 +0200

    DerpQuest: Allow customizing footer text [2/2]

--- src/com/derpquest/settings/fragments/QuickSettings.java
+++ src/com/derpquest/settings/fragments/QuickSettings.java
@@ -16,16 +16,30 @@
 
 package com.derpquest.settings.fragments;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.SearchIndexableResource;
-import androidx.preference.Preference;
+import android.provider.Settings;
 
-import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import androidx.preference.ListPreference;
+import androidx.preference.SwitchPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+
 import com.android.settings.R;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.SearchIndexable;
 import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.derp.support.preference.SystemSettingEditTextPreference;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,6 +48,10 @@ import java.util.List;
 @SearchIndexable
 public class QuickSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
+
+    private static final String QS_FOOTER_TEXT_STRING = "qs_footer_text_string";
+
+    private SystemSettingEditTextPreference mFooterString;
 
     @Override
     public int getMetricsCategory() {
@@ -44,10 +62,35 @@ public class QuickSettings extends SettingsPreferenceFragment implements
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.quicksettings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mFooterString = (SystemSettingEditTextPreference) findPreference(QS_FOOTER_TEXT_STRING);
+        mFooterString.setOnPreferenceChangeListener(this);
+        String footerString = Settings.System.getString(getContentResolver(),
+                QS_FOOTER_TEXT_STRING);
+        if (footerString != null && footerString != "")
+            mFooterString.setText(footerString);
+        else {
+            mFooterString.setText("#StayDerped");
+            Settings.System.putString(getActivity().getContentResolver(),
+                    Settings.System.QS_FOOTER_TEXT_STRING, "#StayDerped");
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mFooterString) {
+            String value = (String) newValue;
+            if (value != "" && value != null)
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.QS_FOOTER_TEXT_STRING, value);
+            else {
+                mFooterString.setText("#StayDerped");
+                Settings.System.putString(getActivity().getContentResolver(),
+                        Settings.System.QS_FOOTER_TEXT_STRING, "#StayDerped");
+            }
+            return true;
+        }
         return false;
     }
 
