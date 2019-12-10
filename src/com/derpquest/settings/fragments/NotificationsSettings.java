0a81cb9c9d84ce1294b23de0843dc803d0bad1ec
00day0 <therandomuser11@gmail.com>
2019-12-11 00:04:32 +0100

    DerpQuest: AmbientLight: update summary to show RGB value instead of ARGB
    Signed-off-by: 00day0 <therandomuser11@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/derpquest/settings/fragments/NotificationsSettings.java
+++ src/com/derpquest/settings/fragments/NotificationsSettings.java
@@ -88,8 +88,9 @@ public class NotificationsSettings extends SettingsPreferenceFragment implements
         mEdgeLightColorPreference.setOnPreferenceChangeListener(this);
         int edgeLightColor = Settings.System.getInt(getContentResolver(),
                 Settings.System.PULSE_AMBIENT_LIGHT_COLOR, 0xFF3980FF);
-        String edgeLightColorHex = String.format("#%08x", (0xFF3980FF & edgeLightColor));
-        if (edgeLightColorHex.equals("#ff3980ff")) {
+
+        String edgeLightColorHex = ColorPickerPreference.convertToRGB(edgeLightColor);
+        if (edgeLightColorHex.equals("#3980ff")) {
             mEdgeLightColorPreference.setSummary(R.string.default_string);
         } else {
             mEdgeLightColorPreference.setSummary(edgeLightColorHex);
@@ -142,9 +143,9 @@ public class NotificationsSettings extends SettingsPreferenceFragment implements
             Settings.System.putInt(getContentResolver(), Settings.System.FLASH_ON_CALLWAITING_DELAY, val);
             return true;
         } else if (preference == mEdgeLightColorPreference) {
-            String hex = ColorPickerPreference.convertToARGB(
+            String hex = ColorPickerPreference.convertToRGB(
                     Integer.valueOf(String.valueOf(newValue)));
-            if (hex.equals("#ff3980ff")) {
+            if (hex.equals("#3980ff")) {
                 preference.setSummary(R.string.default_string);
             } else {
                 preference.setSummary(hex);
