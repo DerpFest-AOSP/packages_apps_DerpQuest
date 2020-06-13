d51ce45219d80b774fa0118a57aaa6ec51edce52
Ido Ben-Hur <idoybh2@gmail.com>
2020-06-13 03:31:47 +0300

    DerpQuest: Add DND & Rate settings to flash on call [3/3]

--- src/com/derpquest/settings/fragments/NotificationsSettings.java
+++ src/com/derpquest/settings/fragments/NotificationsSettings.java
@@ -39,11 +39,12 @@ import com.android.settingslib.search.SearchIndexable;
 import com.derpquest.settings.Utils;
 
 import com.derpquest.settings.preferences.AmbientLightSettingsPreview;
+import com.derpquest.settings.preferences.CustomSeekBarPreference;
 import com.derpquest.settings.preferences.GlobalSettingMasterSwitchPreference;
+import com.derpquest.settings.preferences.SystemSettingListPreference;
 import com.derpquest.settings.preferences.SystemSettingMasterSwitchPreference;
 import com.derpquest.settings.preferences.SystemSettingSeekBarPreference;
-
-import net.margaritov.preference.colorpicker.ColorPickerPreference;
+import com.derpquest.settings.preferences.SystemSettingSwitchPreference;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -58,11 +59,16 @@ public class NotificationsSettings extends SettingsPreferenceFragment implements
     private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
     private static final String PULSE_AMBIENT_LIGHT = "pulse_ambient_light";
     private static final String PREF_HEADS_UP = "heads_up_settings";
+    private static final String PREF_FLASH_ON_CALL = "flashlight_on_call";
+    private static final String PREF_FLASH_ON_CALL_DND = "flashlight_on_call_ignore_dnd";
+    private static final String PREF_FLASH_ON_CALL_RATE = "flashlight_on_call_rate";
 
     private Preference mChargingLeds;
-    private SystemSettingMasterSwitchPreference mFlashOnCall;
     private SystemSettingMasterSwitchPreference mAmbientLight;
     private GlobalSettingMasterSwitchPreference mHeadsUp;
+    private SystemSettingListPreference mFlashOnCall;
+    private SystemSettingSwitchPreference mFlashOnCallIgnoreDND;
+    private CustomSeekBarPreference mFlashOnCallRate;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -105,6 +111,24 @@ public class NotificationsSettings extends SettingsPreferenceFragment implements
         mHeadsUp.setChecked(Settings.Global.getInt(resolver,
                 Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, 1) == 1);
         mHeadsUp.setOnPreferenceChangeListener(this);
+
+        mFlashOnCallRate = (CustomSeekBarPreference)
+                findPreference(PREF_FLASH_ON_CALL_RATE);
+        int value = Settings.System.getInt(resolver,
+                Settings.System.FLASHLIGHT_ON_CALL_RATE, 1);
+        mFlashOnCallRate.setValue(value);
+        mFlashOnCallRate.setOnPreferenceChangeListener(this);
+
+        mFlashOnCallIgnoreDND = (SystemSettingSwitchPreference)
+                findPreference(PREF_FLASH_ON_CALL_DND);
+        value = Settings.System.getInt(resolver,
+                Settings.System.FLASHLIGHT_ON_CALL, 0);
+        mFlashOnCallIgnoreDND.setVisible(value > 1);
+        mFlashOnCallRate.setVisible(value != 0);
+
+        mFlashOnCall = (SystemSettingListPreference)
+                findPreference(PREF_FLASH_ON_CALL);
+        mFlashOnCall.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -119,6 +143,18 @@ public class NotificationsSettings extends SettingsPreferenceFragment implements
             Boolean value = (Boolean) newValue;
             Settings.Global.putInt(resolver,
                     Settings.Global.HEADS_UP_NOTIFICATIONS_ENABLED, value ? 1 : 0);
+            return true;
+        } else if (preference == mFlashOnCall) {
+            int value = Integer.parseInt((String) newValue);
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL, value);
+            mFlashOnCallIgnoreDND.setVisible(value > 1);
+            mFlashOnCallRate.setVisible(value != 0);
+            return true;
+        } else if (preference == mFlashOnCallRate) {
+            int value = (Integer) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.FLASHLIGHT_ON_CALL_RATE, value);
             return true;
         }
         return false;
