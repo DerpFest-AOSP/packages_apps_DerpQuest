27cefd44b3a47cc46b86b0d75ffa958fe6ee73cb
Ido Ben-Hur <idoybh2@gmail.com>
2020-07-13 14:16:47 +0300

    DerpQuest: Make StatusBar master switches summaries dynamic
    Summaries will now show the status of the setting and some info from
    whithin the preference fragment.
    
    Suggested-by: Alexander Brunswig <mralexman3000@gmail.com>

--- src/com/derpquest/settings/fragments/StatusBarSettings.java
+++ src/com/derpquest/settings/fragments/StatusBarSettings.java
@@ -77,6 +77,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
     private SystemSettingMasterSwitchPreference mNotificationTicker;
     private SystemSettingMasterSwitchPreference mBatteryBar;
 
+    private boolean skipSummaryUpdate;
+
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -91,6 +93,7 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         boolean enabled = Settings.System.getInt(resolver,
                 Settings.System.NETWORK_TRAFFIC_STATE, 0) == 1;
         mNetTrafficState.setChecked(enabled);
+        updateNetTrafficSummary(enabled);
 
         mStatusBarClock = (SystemSettingMasterSwitchPreference)
                 findPreference(STATUS_BAR_CLOCK);
@@ -98,43 +101,74 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
         enabled = Settings.System.getInt(resolver,
                 Settings.System.STATUS_BAR_CLOCK, 1) == 1;
         mStatusBarClock.setChecked(enabled);
+        updateClockSummary(enabled);
 
         mStatusBarDate = (SystemSettingMasterSwitchPreference)
                 findPreference(STATUS_BAR_DATE);
         mStatusBarDate.setOnPreferenceChangeListener(this);
         mStatusBarDate.setEnabled(enabled);
-        enabled = Settings.System.getInt(resolver,
-                Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY, 0) > 0;
-        mStatusBarDate.setChecked(enabled);
+        int value = Settings.System.getInt(resolver,
+                Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY, 0);
+        mStatusBarDate.setChecked(value != 0);
+        updateDateSummary(value);
 
         mStatusBarLogo = (SystemSettingMasterSwitchPreference)
                 findPreference(STATUS_BAR_LOGO);
         mStatusBarLogo.setOnPreferenceChangeListener(this);
-        enabled = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_LOGO, 0) > 0;
-        mStatusBarLogo.setChecked(enabled);
+        value = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_LOGO, 0);
+        mStatusBarLogo.setChecked(value != 0);
+        updateLogoSwitchSummary(value);
 
         mBatteryIconStyle = (SystemSettingMasterSwitchPreference)
                 findPreference(BATTERY_ICON_STYLE);
         mBatteryIconStyle.setOnPreferenceChangeListener(this);
-        enabled = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_BATTERY_STYLE, 0) != 6;
-        mBatteryIconStyle.setChecked(enabled);
+        value = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_BATTERY_STYLE, 0);
+        mBatteryIconStyle.setChecked(value != 6);
+        updateBatteryStyleSummary(value);
 
         mNotificationTicker = (SystemSettingMasterSwitchPreference)
                 findPreference(NOTIFICATION_TICKER);
         mNotificationTicker.setOnPreferenceChangeListener(this);
-        enabled = Settings.System.getInt(resolver,
-                Settings.System.STATUS_BAR_SHOW_TICKER, 0) != 0;
-        mNotificationTicker.setChecked(enabled);
+        value = Settings.System.getInt(resolver,
+                Settings.System.STATUS_BAR_SHOW_TICKER, 0);
+        mNotificationTicker.setChecked(value != 0);
+        updateTickerSummary(value);
 
         mBatteryBar = (SystemSettingMasterSwitchPreference)
                 findPreference(BATTERY_BAR);
         mBatteryBar.setOnPreferenceChangeListener(this);
-        enabled = Settings.System.getInt(resolver,
-                Settings.System.BATTERY_BAR_LOCATION, 0) != 0;
-        mBatteryBar.setChecked(enabled);
+        value = Settings.System.getInt(resolver,
+                Settings.System.BATTERY_BAR_LOCATION, 0);
+        mBatteryBar.setChecked(value != 0);
+        updateBatteryBarSummary(value);
 
+        skipSummaryUpdate = true; // avoid being called twice on onResume
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!skipSummaryUpdate) {
+            final ContentResolver resolver = getActivity().getContentResolver();
+            updateNetTrafficSummary(Settings.System.getInt(resolver,
+                    Settings.System.NETWORK_TRAFFIC_STATE, 0) == 1);
+            updateClockSummary(Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_CLOCK, 1) == 1);
+            updateDateSummary(Settings.System.getInt(resolver,
+                    Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY, 0));
+            updateLogoSwitchSummary(Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_LOGO, 0));
+            updateBatteryStyleSummary(Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_BATTERY_STYLE, 0));
+            updateTickerSummary(Settings.System.getInt(resolver,
+                    Settings.System.STATUS_BAR_SHOW_TICKER, 0));
+            updateBatteryBarSummary(Settings.System.getInt(resolver,
+                    Settings.System.BATTERY_BAR_LOCATION, 0));
+        } else {
+            skipSummaryUpdate = false;
+        }
     }
 
     @Override
@@ -143,40 +177,163 @@ public class StatusBarSettings extends SettingsPreferenceFragment implements
             boolean enabled = (boolean) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.NETWORK_TRAFFIC_STATE, enabled ? 1 : 0);
+            updateNetTrafficSummary(enabled);
             return true;
         } else if (preference == mStatusBarClock) {
             boolean enabled = (boolean) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_CLOCK, enabled ? 1 : 0);
             mStatusBarDate.setEnabled(enabled);
+            updateClockSummary(enabled);
             return true;
         } else if (preference == mStatusBarDate) {
             boolean enabled = (boolean) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUSBAR_CLOCK_DATE_DISPLAY, enabled ? 1 : 0);
+            updateDateSummary(enabled ? 1: 0);
             return true;
         } else if (preference == mStatusBarLogo) {
             boolean enabled = (boolean) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_LOGO, enabled ? 1 : 0);
+            updateLogoSwitchSummary(enabled ? 1 : 0);
             return true;
         } else if (preference == mBatteryIconStyle) {
             boolean enabled = (boolean) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_BATTERY_STYLE, enabled ? 0 : 6);
+            updateBatteryStyleSummary(enabled ? 0 : 6);
             return true;
         } else if (preference == mNotificationTicker) {
             boolean enabled = (boolean) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.STATUS_BAR_SHOW_TICKER, enabled ? 1 : 0);
+            updateTickerSummary(enabled ? 1 : 0);
             return true;
         } else if (preference == mBatteryBar) {
             boolean enabled = (boolean) objValue;
             Settings.System.putInt(getActivity().getContentResolver(),
                     Settings.System.BATTERY_BAR_LOCATION, enabled ? 1 : 0);
+            updateBatteryBarSummary(enabled ? 1 : 0);
             return true;
         }
         return false;
+    }
+
+    private void updateNetTrafficSummary(boolean enabled) {
+        Resources res = getResources();
+        int location = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NETWORK_TRAFFIC_VIEW_LOCATION, 0);
+        int style = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.NETWORK_TRAFFIC_TYPE, 0);
+        mNetTrafficState.setSummary(String.format(
+                res.getString(R.string.network_traffic_state_summary),
+                enabled ? res.getString(R.string.on) : res.getString(R.string.off),
+                res.getStringArray(R.array.show_network_traffic_type_entries)[style],
+                res.getStringArray(R.array.network_traffic_location_entries)[location]));
+    }
+
+    private void updateClockSummary(boolean enabled) {
+        Resources res = getResources();
+        int font = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_CLOCK_FONT_STYLE, 28);
+        int location = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUSBAR_CLOCK_STYLE, 0);
+        mStatusBarClock.setSummary(String.format(
+            res.getString(R.string.status_bar_clock_summary),
+            enabled ? res.getString(R.string.on) : res.getString(R.string.off),
+            res.getStringArray(R.array.lock_clock_fonts_entries)[font],
+            res.getStringArray(R.array.status_bar_clock_style_entries)[location]));
+    }
+
+    private void updateDateSummary(int value) {
+        Resources res = getResources();
+        int location = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUSBAR_CLOCK_DATE_POSITION, 0);
+        if (value != 0) {
+            mStatusBarDate.setSummary(String.format(
+                    res.getString(R.string.status_bar_date_summary),
+                    res.getString(R.string.on),
+                    res.getStringArray(R.array.clock_date_display_entries)[value-1],
+                    res.getStringArray(R.array.clock_date_position_entries)[location]));
+        } else {
+            mStatusBarDate.setSummary(String.format(
+                    res.getString(R.string.status_bar_date_summary),
+                    res.getString(R.string.off),
+                    res.getStringArray(R.array.clock_date_display_entries)[0],
+                    res.getStringArray(R.array.clock_date_position_entries)[location]));
+        }
+    }
+
+    private void updateBatteryStyleSummary(int value) {
+        Resources res = getResources();
+        int location = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_SHOW_BATTERY_PERCENT, 0);
+        if (value != 6) {
+            mBatteryIconStyle.setSummary(String.format(
+                    res.getString(R.string.battery_icon_style_summary),
+                    res.getString(R.string.on),
+                    res.getStringArray(R.array.status_bar_battery_style_entries)[value],
+                    res.getStringArray(R.array.battery_percent_entries)[location]));
+        } else {
+            mBatteryIconStyle.setSummary(String.format(
+                    res.getString(R.string.battery_icon_style_summary),
+                    res.getString(R.string.off),
+                    res.getStringArray(R.array.status_bar_battery_style_entries)[0],
+                    res.getStringArray(R.array.battery_percent_entries)[location]));
+        }
+    }
+
+    private void updateLogoSwitchSummary(int value) {
+        Resources res = getResources();
+        int style = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_LOGO_STYLE, 0);
+        if (value != 0) {
+            mStatusBarLogo.setSummary(String.format(
+                    res.getString(R.string.status_bar_logo_category_summary),
+                    res.getString(R.string.on),
+                    res.getStringArray(R.array.status_bar_logo_style_entries)[style],
+                    res.getStringArray(R.array.status_bar_logo_entries)[value-1]));
+        } else {
+            mStatusBarLogo.setSummary(String.format(
+                    res.getString(R.string.status_bar_logo_category_summary),
+                    res.getString(R.string.off),
+                    res.getStringArray(R.array.status_bar_logo_style_entries)[style],
+                    res.getStringArray(R.array.status_bar_logo_entries)[0]));
+        }
+    }
+
+    private void updateTickerSummary(int value) {
+        Resources res = getResources();
+        int animation = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_ANIMATION_MODE, 0);
+        int duration = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.STATUS_BAR_TICKER_TICK_DURATION, 0);
+        mNotificationTicker.setSummary(String.format(
+                res.getString(R.string.status_bar_show_ticker_summary),
+                value != 0 ? res.getString(R.string.on) : res.getString(R.string.off),
+                value == 2 ? res.getString(R.string.on) : res.getString(R.string.off),
+                res.getStringArray(R.array.ticker_animation_mode_entries)[animation],
+                String.valueOf(duration)));
+    }
+
+    private void updateBatteryBarSummary(int value) {
+        Resources res = getResources();
+        int style = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.BATTERY_BAR_STYLE, 0);
+        if (value != 0) {
+            mBatteryBar.setSummary(String.format(
+                    res.getString(R.string.status_bar_logo_category_summary),
+                    res.getString(R.string.on),
+                    res.getStringArray(R.array.battery_bar_style_entries)[style],
+                    res.getStringArray(R.array.battery_bar_entries)[value-1]));
+        } else {
+            mBatteryBar.setSummary(String.format(
+                    res.getString(R.string.status_bar_logo_category_summary),
+                    res.getString(R.string.off),
+                    res.getStringArray(R.array.battery_bar_style_entries)[style],
+                    res.getStringArray(R.array.battery_bar_entries)[0]));
+        }
     }
 
     @Override
