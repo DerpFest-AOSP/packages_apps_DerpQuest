3e270e80aafc30436cf3fa39483b639917053dd7
Ido Ben-Hur <idoybh2@gmail.com>
2020-07-31 21:07:43 +0300

    DerpQuest: Convert PowerMenu background filters to be a master switch
    * New fragment of its own
    * Follow dynamic summary
    Doing this because we may add more settings to it

--- src/com/derpquest/settings/fragments/PowerMenuSettings.java
+++ src/com/derpquest/settings/fragments/PowerMenuSettings.java
@@ -19,11 +19,13 @@ import android.content.Context;
 import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.UserInfo;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.os.UserManager;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.preference.ListPreference;
@@ -40,6 +42,7 @@ import com.android.settings.search.Indexable;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.derpquest.settings.preferences.SystemSettingMasterSwitchPreference;
 import com.derpquest.settings.Utils;
 
 import java.util.Arrays;
@@ -50,9 +53,15 @@ import java.util.List;
 public class PowerMenuSettings extends SettingsPreferenceFragment
                 implements Preference.OnPreferenceChangeListener, Indexable {
 
+    private static final String TAG = "PowerMenuSettings";
+
     private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+    private static final String KEY_POWER_MENU_BG = "power_menu_bg";
 
     private SwitchPreference mPowermenuTorch;
+    private SystemSettingMasterSwitchPreference mPowerMenuBg;
+
+    private boolean skipSummaryUpdate;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -71,17 +80,61 @@ public class PowerMenuSettings extends SettingsPreferenceFragment
         mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
                 Settings.System.POWERMENU_TORCH, 0) == 1));
         }
+
+        mPowerMenuBg = (SystemSettingMasterSwitchPreference) findPreference(KEY_POWER_MENU_BG);
+        mPowerMenuBg.setOnPreferenceChangeListener(this);
+        boolean enabled = Settings.System.getInt(resolver,
+                Settings.System.POWER_MENU_BG, 0) == 1;
+        mPowerMenuBg.setChecked(enabled);
+
+        updatePowerMenuBgSummary(enabled);
+        skipSummaryUpdate = true;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!skipSummaryUpdate) {
+            updatePowerMenuBgSummary(
+                    Settings.System.getInt(getActivity().getContentResolver(),
+                    Settings.System.POWER_MENU_BG, 0) == 1);
+        } else {
+            skipSummaryUpdate = false;
+        }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mPowermenuTorch) {
             boolean value = (Boolean) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
+            Settings.System.putInt(resolver,
                     Settings.System.POWERMENU_TORCH, value ? 1 : 0);
+            return true;
+        } else if (preference == mPowerMenuBg) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.POWER_MENU_BG, value ? 1 : 0);
+            updatePowerMenuBgSummary(value);
             return true;
         }
         return false;
+    }
+
+    private void updatePowerMenuBgSummary(boolean enabled) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final Resources res = getResources();
+        int filter = Settings.System.getInt(resolver,
+                Settings.System.POWER_MENU_BG_STYLE, 0);
+        try {
+            mPowerMenuBg.setSummary(String.format(
+                    res.getString(R.string.power_menu_bg_summary),
+                    enabled ? res.getString(R.string.on) : res.getString(R.string.off),
+                    res.getStringArray(R.array.power_menu_bg_style_entries)[filter]));
+        } catch (Exception e) {
+            Log.e(TAG, "Translation error in power_menu_bg_summary");
+            mPowerMenuBg.setSummary(res.getString(R.string.translation_error));
+        }
     }
 
     @Override
