ce072df359d20ee19da97ec64aab2959825155a0
Ido Ben-Hur <idoybh2@gmail.com>
2020-08-31 06:18:49 +0300

    DerpQuest: Hide corner radius for devices that have a custom cutout

--- src/com/derpquest/settings/fragments/ThemeSettings.java
+++ src/com/derpquest/settings/fragments/ThemeSettings.java
@@ -102,11 +102,17 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
 
         // Rounded Corner Radius
         mCornerRadius = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_SIZE);
-        int resourceIdRadius = (int) ctx.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
-        int cornerRadius = Settings.Secure.getIntForUser(ctx.getContentResolver(), Settings.Secure.SYSUI_ROUNDED_SIZE,
-                ((int) (resourceIdRadius / density)), UserHandle.USER_CURRENT);
-        mCornerRadius.setValue(cornerRadius);
-        mCornerRadius.setOnPreferenceChangeListener(this);
+        boolean hasCustomCutout = res.getBoolean(res.getIdentifier(
+                "config_customCutout", "bool", "com.android.systemui"));
+        if (!hasCustomCutout) {
+            int resourceIdRadius = (int) ctx.getResources().getDimension(com.android.internal.R.dimen.rounded_corner_radius);
+            int cornerRadius = Settings.Secure.getIntForUser(ctx.getContentResolver(), Settings.Secure.SYSUI_ROUNDED_SIZE,
+                    ((int) (resourceIdRadius / density)), UserHandle.USER_CURRENT);
+            mCornerRadius.setValue(cornerRadius);
+            mCornerRadius.setOnPreferenceChangeListener(this);
+        } else {
+            mCornerRadius.setVisible(false);
+        }
 
         // Rounded Content Padding
         mContentPadding = (CustomSeekBarPreference) findPreference(SYSUI_ROUNDED_CONTENT_PADDING);
