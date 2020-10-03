c326942328229cbdca798a8d7069aeea5deae282
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-03 15:43:31 +0300

    DerpQuest: Add pref for qs brightness slider position [2/2]

--- src/com/derpquest/settings/fragments/QuickSettings.java
+++ src/com/derpquest/settings/fragments/QuickSettings.java
@@ -17,6 +17,7 @@
 package com.derpquest.settings.fragments;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
@@ -40,6 +41,7 @@ import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 
 import com.derp.support.preference.SystemSettingEditTextPreference;
+import com.derp.support.preference.SystemSettingMasterSwitchPreference;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -50,8 +52,10 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String QS_FOOTER_TEXT_STRING = "qs_footer_text_string";
+    private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
 
     private SystemSettingEditTextPreference mFooterString;
+    private SystemSettingMasterSwitchPreference mBrightnessSlider;
 
     @Override
     public int getMetricsCategory() {
@@ -63,32 +67,46 @@ public class QuickSettings extends SettingsPreferenceFragment implements
         super.onCreate(icicle);
         addPreferencesFromResource(R.xml.quicksettings);
         PreferenceScreen prefSet = getPreferenceScreen();
+        ContentResolver resolver = getActivity().getContentResolver();
+
+        mBrightnessSlider = (SystemSettingMasterSwitchPreference)
+                findPreference(BRIGHTNESS_SLIDER);
+        mBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean enabled = Settings.System.getInt(resolver,
+                BRIGHTNESS_SLIDER, 1) == 1;
+        mBrightnessSlider.setChecked(enabled);
 
         mFooterString = (SystemSettingEditTextPreference) findPreference(QS_FOOTER_TEXT_STRING);
         mFooterString.setOnPreferenceChangeListener(this);
-        String footerString = Settings.System.getString(getContentResolver(),
+        String footerString = Settings.System.getString(resolver,
                 QS_FOOTER_TEXT_STRING);
         if (footerString != null && footerString != "")
             mFooterString.setText(footerString);
         else {
             mFooterString.setText("#StayDerped");
-            Settings.System.putString(getActivity().getContentResolver(),
+            Settings.System.putString(resolver,
                     Settings.System.QS_FOOTER_TEXT_STRING, "#StayDerped");
         }
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mFooterString) {
             String value = (String) newValue;
             if (value != "" && value != null)
-                Settings.System.putString(getActivity().getContentResolver(),
+                Settings.System.putString(resolver,
                         Settings.System.QS_FOOTER_TEXT_STRING, value);
             else {
                 mFooterString.setText("#StayDerped");
-                Settings.System.putString(getActivity().getContentResolver(),
+                Settings.System.putString(resolver,
                         Settings.System.QS_FOOTER_TEXT_STRING, "#StayDerped");
             }
+            return true;
+        } else if (preference == mBrightnessSlider) {
+            Boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    BRIGHTNESS_SLIDER, value ? 1 : 0);
             return true;
         }
         return false;
