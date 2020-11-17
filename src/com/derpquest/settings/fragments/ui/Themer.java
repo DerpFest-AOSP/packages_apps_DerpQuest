23c4c29cfed305d057c725bd59cc6bb8c6c52f30
Abhat27 <advaithbhat9@gmail.com>
2020-11-17 13:35:35 +0530

    DerpQuest: Bring back Themer
    Co-authored-by: AshutoshSundresh <62063875+AshutoshSundresh@users.noreply.github.com>
    Co-authored-by: pimpmaneaton <shaunsibu@gmail.com>
    Co-authored-by: rezaadi0105 <rezaadipangestu5@gmail.com>
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- src/com/derpquest/settings/fragments/ui/Themer.java
+++ src/com/derpquest/settings/fragments/ui/Themer.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -16,33 +16,38 @@
 
 package com.derpquest.settings.fragments.ui;
 
+import android.content.ContentResolver;
 import android.content.Context;
-import android.os.Bundle;
-import android.provider.SearchIndexableResource;
-import android.provider.Settings;
-import androidx.preference.Preference;
-import androidx.preference.Preference.OnPreferenceChangeListener;
+import android.os.SystemProperties;
+import androidx.preference.*;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.display.OverlayCategoryPreferenceController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 
 import com.android.settings.R;
-import com.android.settings.search.BaseSearchIndexProvider;
-import com.android.settingslib.search.Indexable;
-import com.android.settingslib.search.SearchIndexable;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.Utils;
-
-import com.android.internal.logging.nano.MetricsProto;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@SearchIndexable
-public class Themer extends SettingsPreferenceFragment implements
-        Preference.OnPreferenceChangeListener, Indexable {
+public class Themer extends DashboardFragment {
+    private static final String TAG = "Themer";
+
+    private ContentResolver mResolver;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.themer);
+    public int getMetricsCategory() {
+        return MetricsEvent.DERP;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -50,26 +55,22 @@ public class Themer extends SettingsPreferenceFragment implements
     }
 
     @Override
-    public int getMetricsCategory() {
-        return MetricsProto.MetricsEvent.DERP;
+    protected int getPreferenceScreenResId() {
+        return R.xml.themer;
     }
 
-    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
-        new BaseSearchIndexProvider() {
-            @Override
-            public List<SearchIndexableResource> getXmlResourcesToIndex(Context context,
-                    boolean enabled) {
-                final ArrayList<SearchIndexableResource> result = new ArrayList<>();
-                final SearchIndexableResource sir = new SearchIndexableResource(context);
-                sir.xmlResId = R.xml.themer;
-                result.add(sir);
-                return result;
-            }
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
 
-            @Override
-            public List<String> getNonIndexableKeys(Context context) {
-                final List<String> keys = super.getNonIndexableKeys(context);
-                return keys;
-            }
-    };
-}
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.accent_color"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
+        return controllers;
+    }
+} 
