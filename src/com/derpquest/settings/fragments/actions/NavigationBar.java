fda45bfde3dcc639a7bf84cbbdfdf19a92f796f5
NurKeinNeid <mralexman3000@gmail.com>
2021-01-11 21:30:25 +0100

    Revert "DerpQuest: Add navigation bar visibility control [2/2]"
    This reverts commit b1d7c7011fcf127ea3fcfc371b415ea7b2bcb540.

--- src/com/derpquest/settings/fragments/actions/NavigationBar.java
+++ src/com/derpquest/settings/fragments/actions/NavigationBar.java
@@ -16,10 +16,8 @@
 
 package com.derpquest.settings.fragments.actions;
 
-import android.content.ContentResolver;
 import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
 import android.provider.SearchIndexableResource;
 import android.provider.Settings;
 import androidx.preference.*;
@@ -33,7 +31,6 @@ import com.android.settings.Utils;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.internal.util.derp.derpUtils;
-import com.android.internal.util.derp.ActionUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -42,18 +39,13 @@ import java.util.List;
 public class NavigationBar extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener, Indexable {
 
-    private static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
     private static final String LAYOUT_SETTINGS = "navbar_layout_views";
     private static final String NAVIGATION_BAR_INVERSE = "navbar_inverse_layout";
-    private static final String NAVBAR_VISIBILITY = "navbar_visibility";
+    private static final String PIXEL_NAV_ANIMATION = "pixel_nav_animation";
 
-    private SwitchPreference mPixelNavAnimation;
     private Preference mLayoutSettings;
     private SwitchPreference mSwapNavButtons;
-    private SwitchPreference mNavbarVisibility;
-
-    private boolean mIsNavSwitchingMode = false;
-    private Handler mHandler;
+    private SwitchPreference mPixelNavAnimation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -65,47 +57,14 @@ public class NavigationBar extends SettingsPreferenceFragment implements
         mPixelNavAnimation = (SwitchPreference) findPreference(PIXEL_NAV_ANIMATION);
         mLayoutSettings = (Preference) findPreference(LAYOUT_SETTINGS);
         mSwapNavButtons = (SwitchPreference) findPreference(NAVIGATION_BAR_INVERSE);
-        mNavbarVisibility = (SwitchPreference) findPreference(NAVBAR_VISIBILITY);
 
         if (!derpUtils.isThemeEnabled("com.android.internal.systemui.navbar.threebutton")) {
             prefScreen.removePreference(mLayoutSettings);
         }
-
-        boolean defaultToNavigationBar = ActionUtils.deviceSupportNavigationBar(getActivity());
-        boolean showing = Settings.System.getInt(getContentResolver(),
-                Settings.System.FORCE_SHOW_NAVBAR,
-                defaultToNavigationBar ? 1 : 0) != 0;
-        updateBarVisibleAndUpdatePrefs(showing);
-
-        mNavbarVisibility.setOnPreferenceChangeListener(this);
-
-        mHandler = new Handler();
     }
 
-    @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference.equals(mNavbarVisibility)) {
-            if (mIsNavSwitchingMode) {
-                return false;
-            }
-            mIsNavSwitchingMode = true;
-            boolean showing = ((Boolean)newValue);
-            Settings.System.putInt(getContentResolver(), Settings.System.FORCE_SHOW_NAVBAR,
-                    showing ? 1 : 0);
-            updateBarVisibleAndUpdatePrefs(showing);
-            mHandler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    mIsNavSwitchingMode = false;
-                }
-            }, 1500);
-            return true;
-        }
         return false;
-    }
-
-    private void updateBarVisibleAndUpdatePrefs(boolean showing) {
-        mNavbarVisibility.setChecked(showing);
     }
 
     @Override
